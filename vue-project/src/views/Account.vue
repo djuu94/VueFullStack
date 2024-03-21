a0714200e57b663577fb9bc539c2f48def2c9f8a
djuu94 <86644647+djuu94@users.noreply.github.com>
2024-03-21 19:48:12 +0100

    Add files via upload

---
+++ vue-project/src/views/Account.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Create Account</h1>
+    INSIDE THE ACCOUNT.VUE
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Account',
+});
+</script>
